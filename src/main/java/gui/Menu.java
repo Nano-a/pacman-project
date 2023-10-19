f2302fd88c1a7c56884c55305eb6da8aaf54a9e0
CHIKHI zahoua <chikhizahoua@gmail.com>
2023-10-19 22:41:11 +0200

    quelques ajouts sur le menu principal + menu parametres

--- src/main/java/gui/Menu.java
+++ src/main/java/gui/Menu.java
@@ -1,49 +1,112 @@
+
+
 package gui;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 
-public class Menu extends VBox{
+
+public class Menu extends StackPane {
+    /*
+     * On a apporté une modification à la classe "menu"
+     *  en faisant en sorte qu'elle étende la classe
+     *  StackPane plutôt que VBox. Cette décision découle 
+     * de notre besoin de superposer des boutons sur une 
+     * image de fond. Pour clarifier, voici la distinction
+     *  entre StackPane et VBox :
+     * 
+     * // petite explication ( commentaire à supprimer ) :
+      StackPane : C'est un conteneur de mise en page dans JavaFX 
+    qui empile ses noeuds enfants les uns par-dessus les autres.
+     Dans un StackPane, les noeuds enfants se superposent, 
+     et le dernier noeud ajouté est affiché au-dessus des autres.
+ 
+      VBox : En revanche, VBox est un autre conteneur
+     de mise en page JavaFX, mais il adopte une mise en 
+     page verticale. Dans un VBox, les noeuds enfants sont 
+     disposés verticalement les uns sous les autres, avec
+      un arrangement ordonné de haut en bas.
+     
+     */
 
     private Button jouer;
     private Button parametres;
     private Button quitter;
 
+    public Menu() {
+        //chargement de l'image de fond
+        Image backgroundImage = new Image(getClass().getResourceAsStream("/menuPrincipal.png"));
+        ImageView backgroundImageView = new ImageView(backgroundImage);
 
 
-    public Menu(){
+        // Création d'un VBox pour organiser les boutons
+        VBox buttonContainer = new VBox();
         jouer = new Button("Jouer");
-        parametres =new  Button ("parametres");
-        quitter = new Button ("Quitter");
+        parametres = new Button("Paramètres");
+        quitter = new Button("Quitter");
 
-        /*jouer.setOnAction(e ->{
-            // que faut-il actionner quand on appui sur jouer ?
-        });
+          //images des bouttons :
 
-        parametres.setOnAction(e ->{
-            // que faut-il actionner quand on appui sur jouer ?
-        });
+        Image parametresImage = new Image(getClass().getResourceAsStream("/parametres.png"));
+        ImageView parametresImageView = new ImageView(parametresImage);
+        parametresImageView.setFitWidth(50); 
+        parametresImageView.setFitHeight(50);
+
+        parametres.setGraphic(parametresImageView);
 
 
-         */
+         Image jouerImage = new Image(getClass().getResourceAsStream("/jouer.png"));
+        ImageView jouerImageView = new ImageView(jouerImage);
+        jouerImageView.setFitWidth(50); 
+        jouerImageView.setFitHeight(50);
 
-        
-        getChildren().addAll(jouer,parametres,quitter);
-        setAlignment(Pos.CENTER);
+        jouer.setGraphic(jouerImageView);
 
-        // Espacement entre les boutons 
-        setSpacing(10);
+
+         Image quitterImage = new Image(getClass().getResourceAsStream("/quitter.png"));
+        ImageView quitterImageView = new ImageView(quitterImage);
+        quitterImageView.setFitWidth(50); 
+        quitterImageView.setFitHeight(50);
+
+        quitter.setGraphic(quitterImageView);
+
+      
+
+        // Ajout des boutons au VBox
+        buttonContainer.getChildren().addAll(jouer, parametres, quitter);
+
+        // Configuration de l'espacement vertical entre les boutons
+        buttonContainer.setSpacing(10);
+
+        // Aligner le VBox au centre du StackPane, à la fois horizontalement et verticalement
+        StackPane.setAlignment(buttonContainer, Pos.CENTER);
+
+        // Ajouter des marges pour ajuster l'espacement par rapport à l'image de fond
+        StackPane.setMargin(buttonContainer, new Insets(300));
+
+        // Ajout de l'image de fond, du VBox contenant les boutons dans le StackPane
+        getChildren().addAll(backgroundImageView, buttonContainer);
     }
 
     public Button getJouerButton() {
         return jouer;
     }
 
-    public Button getQuitterButton(){
-        return quitter;
+    public Button getParametresButton(){
+        return parametres;
     }
 
+    public Button getQuitterButton() {
+        return quitter;
+    }
+} 
 
 
-}
+
+
+
