3aa3d0a7fd6a9a1a124b86ff2e0fb511c20e3f18
ajinou <ajinou2001@gmail.com>
2023-11-08 20:39:35 +0100

    Code Blinky

--- src/main/java/model/Ghost.java
+++ src/main/java/model/Ghost.java
@@ -1,14 +1,59 @@
 package model;
 
 import geometry.RealCoordinates;
+import java.util.Random;
 
 public enum Ghost implements Critter {
 
-    // TODO: implement a different AI for each ghost, according to the description in Wikipedia's page
-    BLINKY, INKY, PINKY, CLYDE;
+    BLINKY {
+        @Override
+        public Direction decideNextDirection(MazeState maze, PacMan pacMan) {
+            // Get Blinky's current position
+            RealCoordinates blinkyPos = this.getPos();
+            // Get Pac-Man's current position
+            RealCoordinates pacManPos = pacMan.getPos();
+            
+            // Determine the horizontal and vertical distances from Blinky to Pac-Man
+            double diffX = pacManPos.x() - blinkyPos.x();
+            double diffY = pacManPos.y() - blinkyPos.y();
+            
+            // Determine whether to prioritize horizontal or vertical movement
+            // (Here, we simply choose the direction with the larger distance)
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+                // Move horizontally
+                return diffX > 0 ? Direction.EAST : Direction.WEST;
+            } else {
+                // Move vertically
+                return diffY > 0 ? Direction.SOUTH : Direction.NORTH;
+            }
+        }
+    },
+    INKY {
+        @Override
+        public Direction decideNextDirection(MazeState maze, PacMan pacMan) {
+            // Inky's AI logic goes here
+            return Direction.NONE; // Placeholder
+        }
+    },
+    PINKY {
+        @Override
+        public Direction decideNextDirection(MazeState maze, PacMan pacMan) {
+            // Pinky's AI logic goes here
+            return Direction.NONE; // Placeholder
+        }
+    },
+    CLYDE {
+        @Override
+        public Direction decideNextDirection(MazeState maze, PacMan pacMan) {
+            // Clyde's AI logic goes here
+            return Direction.NONE; // Placeholder
+        }
+    };
 
     private RealCoordinates pos;
     private Direction direction = Direction.NONE;
+    
+    // Common methods for all ghosts
     @Override
     public RealCoordinates getPos() {
         return pos;
@@ -20,8 +65,8 @@ public enum Ghost implements Critter {
     }
 
     @Override
-    public void setDirection(Direction direction) {
-        this.direction = direction;
+    public void setDirection(Direction newDirection) {
+        direction = newDirection;
     }
 
     @Override
@@ -31,7 +76,10 @@ public enum Ghost implements Critter {
 
     @Override
     public double getSpeed() {
-        return 0;
+        return 0; // This should probably be changed to the actual speed of ghosts
     }
 
+    // New method that each Ghost will implement for its AI
+    public abstract Direction decideNextDirection(MazeState maze, PacMan pacMan);
+    
 }
