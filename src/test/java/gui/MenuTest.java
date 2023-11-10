373f9e8369905a19907c0a7f801aa3ff8d603467
CHIKHI zahoua <chikhizahoua@gmail.com>
2023-11-10 14:54:05 +0100

    remise de l'ancienne version

---
+++ src/test/java/gui/MenuTest.java
@@ -0,0 +1,33 @@
+/*mport static org.junit.Assert.*;
+import java.util.logging.*;
+
+public class MenuTest {
+    private static final Logger LOGGER = Logger.getLogger(MenuTest.class.getName());
+
+    @Test
+    public void testGetJouerButton() {
+        Menu menu = new Menu();
+        Button jouerButton = menu.getJouerButton();
+        assertNotNull(jouerButton);
+        assertEquals("Jouer", jouerButton.getText());
+        LOGGER.log(Level.INFO, "Jouer button text is: " + jouerButton.getText());
+    }
+
+    @Test
+    public void testGetParametresButton() {
+        Menu menu = new Menu();
+        Button parametresButton = menu.getParametresButton();
+        assertNotNull(parametresButton);
+        assertEquals("Paramètres", parametresButton.getText());
+        LOGGER.log(Level.INFO, "Parametres button text is: " + parametresButton.getText());
+    }
+
+    @Test
+    public void testGetQuitterButton() {
+        Menu menu = new Menu();
+        Button quitterButton = menu.getQuitterButton();
+        assertNotNull(quitterButton);
+        assertEquals("Quitter", quitterButton.getText());
+        LOGGER.log(Level.INFO, "Quitter button text is: " + quitterButton.getText());
+    }
+}*/
---
+++ src/test/java/gui/ParametresMenuTest.java
@@ -0,0 +1,3 @@
+/*public class ParametresMenuTest {
+    import org.junit.jupiter.api.Test;
+} */
