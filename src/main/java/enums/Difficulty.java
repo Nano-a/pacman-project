8a8d5ac895a7789e140fc3d4f1185e020b75d932
Starkfire3 <134976052+Starkfire3@users.noreply.github.com>
2023-11-23 21:44:35 +0100

    Ajout des énumérations GamesStates, Difficulty et TileType pour la gestion d'états, de difficulté et de type des tuiles dans le jeu

---
+++ src/main/java/enums/Difficulty.java
@@ -0,0 +1,28 @@
+package enums;
+
+import rules.GeneralRules;
+    /**
+     * Representing the games difficulty levels, high score uses this to build itself.
+     */
+    public enum Difficulty {
+        SLOW(0.7),
+        NORMAL(1),
+        FAST(2.25);
+
+        private double speedScale;
+        Difficulty(double d) { this.speedScale = d; }
+
+        /**
+         * @return speed scale for given difficulty.
+         */
+        public double getSpeedScale() { return speedScale; }
+
+        /**
+         * @return timing scale for given difficulty.
+         */
+        public double getTimingScale() {
+            return GeneralRules.SCALE_RULE_TIMERS_WITH_SPEED ? 1 / speedScale : 1;
+        }
+    }
+
+
