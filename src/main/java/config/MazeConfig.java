453a1661e77a1e4794e65fcbf215f812320e9af0
CHERCHOUR naim <naim.cherchour@etu.u-paris.fr>
2024-01-06 13:51:44 +0100

    MazeConfig pour charger le labyrinthe du fichier txt dans resources levels/...

--- src/main/java/config/MazeConfig.java
+++ src/main/java/config/MazeConfig.java
@@ -1,140 +1,162 @@
 package config;
 
 import geometry.IntCoordinates;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import enums.Cell;
+import enums.Direction;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class MazeConfig {
 
-    private  Cell[][] grid;
-    private  IntCoordinates pacManPos, blinkyPos, pinkyPos, inkyPos, clydePos;
+    private int rows; // le nombre de lignes dans le fichier texte du labyrinthe
+    private int cols; // le nombre de colonnes dans le fichier texte du labyrinthe
+    private int nbDots = 0 ; // le nombre de bananes dans le labyrinthe
+    public Cell[][] grid; // la grille de cellules du labyrinthe ; le maze
+    public IntCoordinates pacManPos;
+    public IntCoordinates lionPos;
+    public IntCoordinates gorillaPos;
+    public IntCoordinates snakePos;
+    public IntCoordinates tigerPos;
+    public IntCoordinates pacmanDirection;
+    public IntCoordinates lionDirection;
+    public IntCoordinates gorillaDirection;
+    public IntCoordinates snakeDirection;
+    public IntCoordinates tigerDirection;
 
-    // constructeur
-    public MazeConfig(Cell[][] grid, IntCoordinates pacManPos, IntCoordinates blinkyPos, IntCoordinates pinkyPos,
-                      IntCoordinates inkyPos, IntCoordinates clydePos) {
-        this.grid = grid ;
-        this.pacManPos = pacManPos;
-        this.blinkyPos = blinkyPos;
-        this.inkyPos = inkyPos;
-        this.pinkyPos = pinkyPos;
-        this.clydePos = clydePos;
-    }
-
-
-    // getters
+    // getters et pas besoin de setters,on ne va pas écrire sur les fichiers
+    public int getRows() {return rows;}
+    public void setRows(int rows) {this.rows = rows;}
+    public int getCols() {return cols;}
+    public void setCols(int cols) {this.cols = cols;}
+    public int getNbDots() {return nbDots;}
+    public void setNbDots(int nbDots) {this.nbDots = nbDots;}
     public IntCoordinates getPacManPos() {
         return pacManPos;
     }
-
-    public IntCoordinates getBlinkyPos() {
-        return blinkyPos;
+    public IntCoordinates getLionPos() {return lionPos;}
+    public IntCoordinates getGorillaPos() {
+        return gorillaPos;
+    }
+    public IntCoordinates getSnakePos() {
+        return snakePos;
+    }
+    public IntCoordinates getTigerPos() {
+        return tigerPos;
     }
 
-    public IntCoordinates getPinkyPos() {
-        return pinkyPos;
+   // no constructor no need
+    public MazeConfig ( String file ) {
+        loadMazeFromFile(file);
     }
 
-    public IntCoordinates getInkyPos() {
-        return inkyPos;
-    }
-
-    public IntCoordinates getClydePos() {
-        return clydePos;
-    }
-
-    public  int getWidth() {
-        return grid[0].length;
-    }
-
-    public  int getHeight() {
-        return grid.length;
-    }
-
-    public Cell getCell(IntCoordinates pos) {
-        return grid[Math.floorMod(pos.y(), getHeight())][Math.floorMod(pos.x(), getWidth())];
-    }
-
-    private MazeConfig loadMazeFromFile() {
-        try (BufferedReader br = new BufferedReader(new FileReader("src/main/java/config/pacman_maze.txt"))) {
-
-            String line;
-            Cell[][] grid = new Cell[31][28];  // to fix to generalise for each text file
-            int row = 0;
-
-            while ((line = br.readLine()) != null) {
-                String[] columns = line.split("");
-
-                for (int col = 0; col < columns.length; col++) {
-                    grid[row][col] = parseCell(columns[col]);
-                }
-
-                row++;
-            }
-
-            // Use the Cell[][] grid to create a MazeConfig
-            IntCoordinates pacManPos = new IntCoordinates(15, 15);
-            IntCoordinates blinkyPos = new IntCoordinates(0, 3);
-            IntCoordinates pinkyPos = new IntCoordinates (3, 5);
-            IntCoordinates inkyPos = new IntCoordinates(5,5);
-            IntCoordinates clydePos = new IntCoordinates(5,1);
-
-            this.grid = grid ;
-            this.pacManPos = pacManPos;
-            this.blinkyPos = blinkyPos;
-            this.inkyPos = inkyPos;
-            this.pinkyPos = pinkyPos;
-            this.clydePos = clydePos;
-
-            return new MazeConfig(grid, pacManPos, blinkyPos, pinkyPos, inkyPos, clydePos);
-        } catch (IOException e) {
+    public void numberOfRowsCol ( File file ){
+        // cette méthode permet de calculer le nombre de lignes et de colonnes du fichier texte du labyrinthe
+        // le principe des niveaux est on initialise le fichier du niveau 1 puis 2 puis 3 , sous réserve de collectionner tous les dots du jeu pour passer à un autre level
+        // c'est géré dans le model
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-            return null;
         }
-    }
-
-    private Cell parseCell(String cellValue) {
-        int value = Integer.parseInt(cellValue);
-        switch (value) {
-            case 0:
-                return Cell.open(Cell.Content.NOTHING);  // Open space
-            case 1:
-                return Cell.closed(Cell.Content.NOTHING); // Closed space
-            case 2:
-                return Cell.open(Cell.Content.DOT);  // Dot
-            case 3:
-                return Cell.open(Cell.Content.ENERGIZER); // Energizer
-            case 4:
-                return Cell.open(Cell.Content.NOTHING); // FIXME
-
-            case 5 :
-                return Cell.open(Cell.Content.NOTHING); // FIXME
-
-            case 6 :
-                return Cell.open(Cell.Content.NOTHING); //FIXME
-
-            case 7 :
-                return Cell.open(Cell.Content.NOTHING); //FIXME
-
-            case 8 :
-                return Cell.open(Cell.Content.NOTHING); //FIXME
-
-            default:
-                return Cell.open(Cell.Content.NOTHING);  //FIXME
+        // on calcule le nombre de lignes et de colonnes du fichier texte du labyrinthe
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            Scanner lineScanner = new Scanner(line);
+            while (lineScanner.hasNext()) {
+                lineScanner.next();
+                cols++;
+            }
+            rows++;
         }
+        cols = cols/rows; // on a le nombre de colonnes par ligne
     }
 
-    public static MazeConfig makeExample1() {
-        // FIXME : static call problem , but we should only call loadMazeFromFile , the first creation of m is useless
-        MazeConfig m  = new MazeConfig(new Cell[8][9], new IntCoordinates(3, 0),
-        new IntCoordinates(15, 15),
-         new IntCoordinates (3, 5),
-        new IntCoordinates(5,5),
-        new IntCoordinates(5,1));
-
-        //labyrinth loaded from the file
-        m.loadMazeFromFile();
-        return m ;
-
+    public void loadMazeFromFile(String fileName) {
+        File file = new File(fileName);
+        numberOfRowsCol(file); // on calcule le nombre de lignes et de colonnes du fichier texte du labyrinthe
+        Scanner scanner2 = null;
+        try {
+            scanner2 = new Scanner(file); // on réinitialise le scanner pour lire le fichier depuis le début
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        grid = new Cell[rows][cols]; // on crée la matrice de cellules ; on a 9 types de cellules ;
+        // une grille de cellules de la classe Cell qui est un enum et chaque Cell correspond en vue à une image
+        // et non pas à un type Rectangle de Javafx comme auparavant avec des walls
+        int row = 0;
+        int pacManRow = 0;int pacManColumn = 0;
+        int lionRow = 0;int lionColumn = 0;
+        int gorillaRow = 0;int gorillaColumn = 0;
+        int snakeRow = 0;int snakeColumn = 0;
+        int tigerRow = 0;int tigerColumn = 0;
+        while(scanner2.hasNextLine()){
+            int column = 0;
+            String line= scanner2.nextLine();
+            Scanner lineScanner = new Scanner(line);
+            while (lineScanner.hasNext()){
+                String charactere  = lineScanner.next();
+                Cell content;
+                if (charactere.equals("T")){
+                    content = Cell.TREE;
+                }
+                else if (charactere.equals("B")){ // banana
+                    content = Cell.DOT;
+                    nbDots++; // on compte le nombre de bananes
+                }
+                else if (charactere.equals("L")){ // Load of banans as an energizer
+                    content = Cell.ENERGIZER;
+                    nbDots++;
+                }
+                else if (charactere.equals("1")){
+                    content = Cell.LION; // la position initiale du lion dans le labyrinthe
+                    lionRow = row;
+                    lionColumn = column;
+                }
+                else if (charactere.equals("2")){
+                    content = Cell.GORILLA;
+                    gorillaRow = row;
+                    gorillaColumn = column;
+                } else if (charactere.equals("3")){
+                    content = Cell.SNAKE;
+                    snakeRow = row;
+                    snakeColumn = column;
+                } else if (charactere.equals("4")){
+                    content = Cell.TIGER;
+                    tigerRow = row;
+                    tigerColumn = column;
+                } else if (charactere.equals("P")){
+                    content = Cell.PACMAN; // la position initiale de PacMan dans le labyrinthe
+                    pacManRow = row;
+                    pacManColumn = column;
+                }
+                else //(value.equals("N")) empty cell
+                {
+                    content = Cell.NOTHING;
+                }
+                grid[row][column] = content; //on remplie la grille
+                column++; //prochaine colonne
+            }
+            row++; //prochaine ligne
+        }
+        // on initilise les positions des entités selon le row et column dans fichier txt
+        pacManPos = new IntCoordinates(pacManRow, pacManColumn);
+        pacmanDirection = new IntCoordinates(0,0);
+        lionPos = new IntCoordinates(lionRow,lionColumn);
+        lionDirection = new IntCoordinates(-1, 0);
+        gorillaPos = new IntCoordinates(gorillaRow,gorillaColumn);
+        gorillaDirection = new IntCoordinates(-1, 0);
+        snakePos = new IntCoordinates(snakeRow,snakeColumn);
+        snakeDirection = new IntCoordinates(-1, 0);
+        tigerPos = new IntCoordinates(tigerRow,tigerColumn);
+        tigerDirection = new IntCoordinates(-1, 0);
     }
+
+    public Cell getCell(int row, int column) {
+        assert row >= 0 && row < this.grid.length && column >= 0 && column < this.grid[0].length;
+        return this.grid[row][column];
+    }
+
 }
