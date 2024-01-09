eb1311092629fce24ae9bbc58a8b7d88895d4efc
GEBRAYEL maroun <maroun.gebrayel@etu.u-paris.fr>
2024-01-09 23:24:25 +0100

    La vue du labyrinthe : initialisation de grille d'ImageView et chargement d'image

--- src/main/java/gui/GameView.java
+++ src/main/java/gui/GameView.java
@@ -1,87 +1,89 @@
-/*
- * Il est au cœur de la visualisation du jeu, gérant l'affichage du labyrinthe,
- * de Pacman, des fantômes, et d'autres éléments du jeu
- */
 package gui;
 
-import geometry.IntCoordinates;
-import javafx.animation.AnimationTimer;
-import javafx.scene.layout.Pane;
-import javafx.scene.control.Label;
+import enums.Cell;
+import enums.Direction;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import model.MazeState;
+import config.MazeConfig;
+import java.util.Objects;
 
-import java.util.ArrayList;
-import java.util.List;
-
-// Cette classe gère l'affichage du jeu, y compris le labyrinthe et les personnages.
-public class GameView {
+/**
+ * Description : gérer la vue graphique du jeu
+ */
+public class GameView extends Group {
+    // extends Group car on va ajouter des éléments graphiques à la vue dans un fichier fxml (GamePage.fxml)
     // class parameters
-    private final MazeState maze; // État actuel du labyrinthe
-    private final Pane gameRoot; // main node of the game
-    // private final Label scoreLabel; // Label to display th
+    public final static double CELL_WIDTH = 30.0; // l'ideale pour ne pas dépasser le frame de l'écran
+    private int rows;
+    private int cols;
+    private MazeConfig maze; // pour récupérer la structure du labyrinthe ; la position de pacman et des ...
+    private ImageView[][] cellImages; // Une grille de 2D pour stocker les images de chaque cellule du labyrinthe
+    // c'est ainsi que le labyrinthe est affiché
 
-    private final List<GraphicsUpdater> graphicsUpdaters;
+    // Images pour les différents éléments du jeu
+    private Image pacmanEastImage;
+    private Image pacmanNorthImage;
+    private Image pacmanSouthImage;
+    private Image pacmanWestImage;
+    private Image lionImage;
+    private Image gorillaImage;
+    private Image snakeImage;
+    private Image tigerImage;
+    private Image bluelionImage;
+    private Image bluegorillaImage;
+    private Image bluesnakeImage;
+    private Image bluetigerImage;
+    private Image treeImage;
+    private Image loadBanana;
+    private Image banana;
 
-    // Méthode pour ajouter un élément graphique à la vue.
-    private void addGraphics(GraphicsUpdater updater) {
-        gameRoot.getChildren().add(updater.getNode());
-        graphicsUpdaters.add(updater);
+    public GameView() {
+        // Initialisation des images à partir des ressources (chargement des images depuis des fichiers)
+
+        this.pacmanEastImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/pacmanRight.gif")));
+        this.pacmanNorthImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/pacmanUp.gif")));
+        this.pacmanSouthImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/pacmanDown.gif")));
+        this.pacmanWestImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/pacmanLeft.gif")));
+
+        this.lionImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/lionRight.gif")));
+        this.bluelionImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/bluelion.gif")));
+
+        this.gorillaImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/GorillaDown.gif")));
+        this.bluegorillaImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/bluegorilla.gif")));
+
+        this.snakeImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/snake.gif")));
+        this.bluesnakeImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/bluesnake.gif")));
+
+        this.tigerImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/tiger.gif")));
+        this.bluetigerImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/bluetiger.gif")));
+
+        this.treeImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/arbre.png")));
+        this.loadBanana = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/energ.png")));
+        this.banana = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/bananaa.png")));
+
     }
 
-    /**
-     * @param maze  le "modèle" de cette vue (le labyrinthe et tout ce qui s'y trouve)
-     * @param root  le nœud racine dans la scène JavaFX dans lequel le jeu sera affiché
-     * @param scale le nombre de pixels représentant une unité du labyrinthe
-     */
-
-     // Constructeur initialisant les éléments nécessaires pour l'affichage.
-    public GameView(MazeState maze, Pane root, double scale) {
-        // Initialisation des usines de graphiques.
-        this.maze = maze;
-        this.gameRoot = root;
-        // Create and configure the score label
-       /* scoreLabel = new Label();
-        scoreLabel.setStyle("-fx-font-size: 20px; -fx-text-fill: white;");
-        scoreLabel.setLayoutY(10); // Adjust the Y position for the label
-        gameRoot.getChildren().add(scoreLabel); // Add the label to the game root */
-        // pixels per cell
-        root.setMinWidth(maze.getWidth() * scale);
-        root.setMinHeight(maze.getHeight() * scale);
-        root.setStyle("-fx-background-color: #000000");
-        var critterFactory = new CritterGraphicsFactory(scale);
-        var cellFactory = new CellGraphicsFactory(scale);
-        graphicsUpdaters = new ArrayList<>();
-        for (var critter : maze.getCritters()) addGraphics(critterFactory.makeGraphics(critter));
-        for (int x = 0; x < maze.getWidth(); x++)
-            for (int y = 0; y < maze.getHeight(); y++)
-                addGraphics(cellFactory.makeGraphics(maze, new IntCoordinates(x, y)));
-
-    }
-
-    public void animate() {
-        new AnimationTimer() {
-            // Mise à jour de la vue.
-            long last = 0;
-
-            @Override
-            public void handle(long now) {
-                if (last == 0) { // ignore the first tick, just compute the first deltaT
-                    last = now;
-                    return;
+    private void initializeGrid() {
+        // Initialisation de la grille de 2D pour stocker les images de chaque cellule du labyrinthe
+        // CELL_WIDTH est la taille d'une cellule en pixels , on peut changer la taille de l'écran du labyrinthe avec
+        if (this.rows > 0 && this.cols > 0) {
+            this.cellImages = new ImageView[this.rows][this.cols];
+            for (int row = 0; row < this.rows; row++) {
+                for (int column = 0; column < this.cols; column++) {
+                    ImageView imageView = new ImageView();
+                    imageView.setX((double) column * CELL_WIDTH);
+                    imageView.setY((double) row * CELL_WIDTH);
+                    imageView.setFitWidth(CELL_WIDTH);
+                    imageView.setFitHeight(CELL_WIDTH);
+                    this.cellImages[row][column] = imageView;
+                    this.getChildren().add(imageView);
                 }
-                var deltaT = now - last;
-                maze.update(deltaT);
-                for (var updater : graphicsUpdaters) {
-                    updater.update();
-                }
-               // updateScoreLabel(); // Update the score label
-                last = now;
             }
-        }.start();
+        }
     }
 
-    /*private void updateScoreLabel() {
-        scoreLabel.setText("Score: " + maze.getScore()); // Update the score label text
-    }*/
+
 
 }
