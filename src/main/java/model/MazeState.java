6da10561639de6bbbfc81439a7b7fd6f9f31db6f
Starkfire3 <134976052+Starkfire3@users.noreply.github.com>
2024-01-09 22:08:54 +0100

    Initialisation de la partie et variable du jeu et passage de niveau

--- src/main/java/model/MazeState.java
+++ src/main/java/model/MazeState.java
@@ -1,176 +1,144 @@
 package model;
 
 import config.MazeConfig;
+import enums.Cell;
 import geometry.IntCoordinates;
-import geometry.RealCoordinates;
-
-import java.util.List;
-import java.util.Map;
-
-// Importations nécessaires pour l'interface utilisateur JavaFX
+import enums.Direction;
+import gui.Controller;
+import gui.SoundController;
+import javafx.animation.PauseTransition;
 import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
+import javafx.util.Duration;
+import java.util.Random;
 
-import static model.Ghost.*;
-
-// Classe finale pour gérer l'état du labyrinthe dans le jeu.
+/**
+ * Description : gérer l'état du labyrinthe dans le jeu et tous le model ; move Pacman et les ghosts
+ * les ghosts qui retournent à leurs positions initiliale , les sorties du labyrinthe , passer au level suivant
+ * Manger les dots et les energizer et biensur les collisions entre les ghosts et Pacman
+ * et le ghost eating mode ou les ghosts peuvent être mangé par Pacman
+ */
 public final class MazeState {
-    private final MazeConfig config; // Configuration du labyrinthe.
-    private final int height; // Hauteur du labyrinthe.
-    private final int width; // Largeur du labyrinthe.
-    private final Label scoreLabel = new Label(); // Étiquette pour afficher le score.
+    private final MazeConfig config; // Configuration du labyrinthe à [partir du fichier]
+    private static boolean gameOver; // variable de jeu pour savoir si on a perdu
+    private static boolean Pause; // variable de jeu pour savoir si on est en pause
+    private static boolean youWon; // variable de jeu pour savoir si on a gagné
+    private static boolean ghostEatingMode;  // vsi on est en ghost eating mode
+    private int score;
+    private int level;
+    private int lives;
+    private static Direction lastDirection;
+    private static Direction currentDirection = Direction.NONE;
 
-    private final boolean[][] gridState; // État des cellules du labyrinthe (occupé ou non).
+    public Direction getLastDirection() {
+        return lastDirection;
+    }
 
-    private final List<Critter> critters; // Liste des créatures (Pac-Man et fantômes).
-    private int score; // Score actuel du joueur.
+    public Direction getCurrentDirection() {
+        return currentDirection;
+    }
 
-    private final Map<Critter, RealCoordinates> initialPos; // Positions initiales des créatures.
-    private int lives = 3; // Nombre de vies du joueur.
+    public void setCurrentDirection(Direction direction) {
+        currentDirection = direction;
+    }
 
-    // Constructeur de MazeState.
+    public int getLives() {
+        return lives;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
     public MazeState(MazeConfig config) {
         this.config = config;
-        height = config.getHeight();
-        width = config.getWidth();
-        scoreLabel.setText("Score: " + score); // Initialisation de l'étiquette du score.
-        VBox vbox = new VBox(); // Boîte verticale pour l'interface utilisateur.
-        vbox.getChildren().add(scoreLabel); // Ajout de l'étiquette du score à la boîte.
-        // Initialisation des créatures et de l'état du labyrinthe.
-        critters = List.of(PacMan.INSTANCE, Ghost.CLYDE, BLINKY, INKY, PINKY);
-        gridState = new boolean[height][width];
-        initialPos = Map.of(
-                PacMan.INSTANCE, config.getPacManPos().toRealCoordinates(1.0),
-                BLINKY, config.getBlinkyPos().toRealCoordinates(1.0),
-                INKY, config.getInkyPos().toRealCoordinates(1.0),
-                CLYDE, config.getClydePos().toRealCoordinates(1.0),
-                PINKY, config.getPinkyPos().toRealCoordinates(1.0)
-        );
-        resetCritters(); // Réinitialisation des positions des créatures.
     }
 
-    public List<Critter> getCritters() {
-        return critters;
+    public void startNewGame() {
+        // initialiser les variables du jeu pour commencer une nouvelle partie
+        this.gameOver = false;
+        this.youWon = false;
+        this.Pause = false;
+        this.ghostEatingMode = false;
+        config.setNbDots(0);
+        config.setRows(0);
+        config.setCols(0);
+        this.score = 0;
+        this.level = 1;
+        this.lives = 3; // 3 vies
+        config.loadMazeFromFile(Controller.getLevelFile(0)); // niveau 1 avec indice dans le tableau 0
     }
 
-    public double getWidth() {
-        return width;
-    }
 
-    public int getHeight() {
-        return height;
-    }
-
-    public void update(long deltaTns) {
-        // FIXME: too many things in this method. Maybe some responsibilities can be delegated to other methods or classes?
-        for  (var critter: critters) {
-            var curPos = critter.getPos();
-            var nextPos = critter.nextPos(deltaTns);
-            var curNeighbours = curPos.intNeighbours();
-            var nextNeighbours = nextPos.intNeighbours();
-            boolean wallCollision = false; // booléan qui va permettre de savoir quand pacman touche un mur
-            if (!curNeighbours.containsAll(nextNeighbours)) {
-                switch (critter.getDirection()) {
-                    case NORTH -> {
-                        for (var n: curNeighbours) {
-                            if (config.getCell(n).northWall()) {
-                                wallCollision = true;
-                                break;
-                            }
-                        }
-                    }
-                    case EAST -> {
-                        for (var n: curNeighbours) {
-                            if (config.getCell(n).eastWall()) {
-                                wallCollision = true;
-                                break;
-                            }
-                        }
-                    }
-                    case SOUTH -> {
-                        for (var n: curNeighbours) {
-                            if (config.getCell(n).southWall()) {
-                                wallCollision = true;
-                                break;
-                            }
-                        }
-                    }
-                    case WEST -> {
-                        for (var n: curNeighbours) {
-                            if (config.getCell(n).westWall()) {
-                                wallCollision = true;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-            // Update position only if there is no wall collision
-            if (!wallCollision) {
-                critter.setPos(nextPos.warp(width, height));
+    public void startNextLevel() {
+        if (this.isLevelComplete()) {
+            this.level++;
+            config.setCols(0);
+            config.setCols(0);
+            youWon = false;
+            ghostEatingMode = false;
+            try {
+                config.loadMazeFromFile(Controller.getLevelFile(level - 1));
+            } catch (ArrayIndexOutOfBoundsException e) {
+                //si y'a plus de niveaux on a fini le 3 c'est à dire on a gagné
+                youWon = true;
+                gameOver = true;
+                level--;
             }
         }
-        // FIXME Pac-Man rules should somehow be in Pacman class
-        var pacPos = PacMan.INSTANCE.getPos().round();
-        if (!gridState[pacPos.y()][pacPos.x()]) {
-            addScore(1);
-            gridState[pacPos.y()][pacPos.x()] = true;
-        }
-        for (var critter : critters) {
-            if (critter instanceof Ghost && critter.getPos().round().equals(pacPos)) {
-                if (PacMan.INSTANCE.isEnergized()) {
-                    addScore(10);
-                    resetCritter(critter);
-                } else {
-                    playerLost();
-                    return;
+    }
+
+    public boolean isLevelComplete() {
+        // c'est à dire que tous les dots ont été mangé par PacMan
+        return config.getNbDots() == 0;
+    }
+
+    public static boolean isGameOver() {
+        return gameOver;
+    }
+
+    public static boolean isPaused() {
+        return Pause;
+    }
+
+    public static boolean isGhostEatingMode() {
+        return ghostEatingMode;
+    }
+
+    public static void setGhostEatingMode(boolean ghostEatingModeBool) {
+        ghostEatingMode = ghostEatingModeBool;
+    }
+
+    public static boolean isYouWon() {
+        return youWon;
+    }
+
+
+    public void resetPositions() {
+        // Réinitialiser la position de Pac-Man à sa position de départ
+        // appelé une fois il a perdu la vie
+        for (int row = 0; row < config.getRows(); row++) {
+            for (int column = 0; column < config.getCols(); column++) {
+                if (config.grid[row][column] == Cell.PACMAN) {
+                    config.pacManPos = new IntCoordinates(row, column);
+                    break;
                 }
             }
         }
+        // Réinitialiser la position des fantômes à leur position de départ
+        sendLionHome();
+        sendGorillaHome();
     }
 
-    // Méthode pour ajouter des points au score.
     private void addScore(int increment) {
         score += increment;
-        displayScore(); // Affichage du score mis à jour.
-    }
-
-    // Méthode pour afficher le score.
-    private void displayScore() {
-        // Affichage du score dans la console (à remplacer par un affichage dans l'interface utilisateur).
-        System.out.println("Score: " + score);
-    }
-
-    // Méthode appelée lorsque le joueur perd une vie.
-    private void playerLost() {
-        lives--;
-        if (lives == 0) {
-            System.out.println("Game over!"); // Affichage du message de fin de jeu.
-            System.exit(0); // Sortie du jeu.
-        }
-        System.out.println("Lives: " + lives); // Affichage des vies restantes.
-        resetCritters(); // Réinitialisation des positions des créatures.
-    }
-
-
-    private void resetCritter(Critter critter) {
-        critter.setDirection(Direction.NONE);
-        critter.setPos(initialPos.get(critter));
-    }
-
-    private void resetCritters() {
-        for (var critter: critters) resetCritter(critter);
     }
 
     public MazeConfig getConfig() {
         return config;
     }
-
-    public boolean getGridState(IntCoordinates pos) {
-        return gridState[pos.y()][pos.x()];
-    }
 }
+
