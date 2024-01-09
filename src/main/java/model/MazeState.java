ab3c8a03f71f0dee4c47eeddf93f7483aed6c169
CHIKHI zahoua <chikhizahoua@gmail.com>
2024-01-09 22:49:12 +0100

    ajout de la methode qui remet le fantome ( lion ) à sa place initiale

--- src/main/java/model/MazeState.java
+++ src/main/java/model/MazeState.java
@@ -191,6 +191,32 @@ public final class MazeState {
         }
     }
     
+    public void sendLionHome() { //lion
+        for (int row = 0; row < config.getRows(); row++) {
+            for (int column = 0; column < config.getCols(); column++) {
+                if (config.grid[row][column] == Cell.LION) {
+                    // on récupère la position du lion initiale
+                    config.lionPos = new IntCoordinates(row, column);
+                }
+            }
+        }
+
+        // Mettez en pause le mouvement du lion
+        config.lionDirection = new IntCoordinates(0, 0);
+
+        // Créez un PauseTransition de 2 secondes
+        PauseTransition pause = new PauseTransition(Duration.seconds(2));
+        pause.setOnFinished(event -> {
+            // Après 2 secondes, remettez le fantôme en mouvement
+            Platform.runLater(() -> {
+                config.lionDirection = new IntCoordinates(-1, 0); // Remplacez par la logique de mouvement initial du fantôme
+            });
+        });
+
+        // Démarrez la transition
+        pause.play();
+    }
+    
     //TODO : Ajouter le 3ème et le 4ème animal avec une IA différente de celle du lion et du gorille
     public void moveGhosts() {
         IntCoordinates[] ghost1Data = moveAGhost(config.lionDirection, config.lionPos);
